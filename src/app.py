704c380906044bce9b0dd60476eab37195563ee2
JoseSoto03 <134469959+JoseSoto03@users.noreply.github.com>
2025-06-12 04:52:42 +0000

    Ejercicio Terminado

--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+artist_id = "7dGJo4pcD2V6oG8kP0tJRR"
+
+top10_tracks = spotify.artist_top_tracks(artist_id, country="US")
+
+print("Top 10 canciones mas populares")
+for idx, track in enumerate(top10_tracks["tracks"][:10]):
+    print(f"{idx+1}. {track['name']} - {track['popularity']} de popularidad")
